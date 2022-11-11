e2efb51a53c83f858217ee12aa430c1e2a254cb0
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-11-11 20:21:29 +0100

    chore: write an article markup for the `pages/post.vue`

---
+++ pages/post.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <article>
+      <h1 class="title">What is balut?</h1>
+      <p>
+        If someone placed balut on your plate, you might think they were serving you a hardboiled egg. That is, until you cracked it open and a fully intact duck embryo spilled out. Balut, considered a delicacy in many Asian countries, is produced when fertilized duck eggs are placed in warm sunlight. After about eight days, the eggs are held up to the light and checked to ensure that the budding embryo is ready. Then, the eggs are cooked and served with a dash of salt and a few squirts of lemon juice.
+      </p>
+    </article>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style scoped></style>
