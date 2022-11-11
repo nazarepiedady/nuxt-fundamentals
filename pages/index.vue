5b3ca2a420b76f7fc86dfbe09fbc607c9f072ed5
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-11-11 20:16:33 +0100

    chore: remove the default content from `pages/index.vue`

--- pages/index.vue
+++ pages/index.vue
@@ -2,27 +2,6 @@
   <div class="container">
     <div>
       <Logo />
-      <h1 class="title">
-        nuxt-fundamentals
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
     </div>
   </div>
 </template>
